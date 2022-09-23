02df98d150fd57e69f1533c91b8d54112666f197
wayne616 <git@github.com:wayne616/GagnoMaxime_7_07042022.git>
2022-09-23 12:31:46 +0200

    update

---
+++ back/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauces');
+
+
+mongoose.connect('mongodb+srv://wayne666:8074Amzl@cluster0.cbw1h.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
+
+
+
+module.exports = app;
